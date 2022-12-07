c727eeae7af5703e72164e95d72af7c1ad14f524
maddealer <forento@gmail.com>
2022-12-07 18:57:30 +0200

    half work

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 import MovieListing from "./MovieListing";
 import MovieApi from "../common/apis/MovieApi";
 import { APIKey } from "../common/apis/MovieApiKey";
+import { addMovies } from "../features/movies/movieSlice";
+import { useDispatch } from "react-redux/es/exports";
 const Home = () => {
+  const movieText = "star";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "star";
     const fetchMovies = async () => {
       const response = await MovieApi.get(
         `?apiKey=${APIKey}&s=${movieText}&type=movie`
       ).catch((err) => {
         console.log("Err : ", err);
       });
+      dispatch(addMovies(response.data));
       console.log(response);
     };
     fetchMovies();
--- src/components/MovieListing.js
+++ src/components/MovieListing.js
@@ -1,7 +1,32 @@
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
-
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../features/movies/movieSlice";
+import MovieCard from "./MovieCard";
+import "./MovieListing.css";
 const MovieListing = () => {
-  return <div>MovieListing</div>;
+  const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+        return <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
