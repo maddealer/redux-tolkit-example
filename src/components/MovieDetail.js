dbe4c132a48306ccf2964bca7d3ae44d7b5b145e
maddealer <forento@gmail.com>
2022-12-10 23:45:48 +0200

    some new

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./MovieDetail.css";
+import {
+  fetchAsyncMovieOrShowsDetails,
+  getDetail,
+} from "./../features/movies/movieSlice";
 
 const MovieDetail = () => {
-  return <div>MovieDetails</div>;
+  const { imdbID } = useParams();
+  const dispatch = useDispatch();
+  const data = useSelector(getDetail);
+  useEffect(() => {
+    dispatch(fetchAsyncMovieOrShowsDetails(imdbID));
+  }, [dispatch, imdbID]);
+  return (
+    <div className="movie-section">
+      <div className="section-left">
+        <div className="movie-title">{data.Title}</div>
+        <div className="movie-rating">
+          <span>
+            IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
+          </span>
+          <span>
+            IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
+          </span>
+          <span>
+            Runtime <i className="fa fa-film"></i> : {data.Runtime}
+          </span>
+          <span>
+            Year <i className="fa fa-calendar"></i> : {data.Year}
+          </span>
+        </div>
+        <div className="movie-plot">{data.Plot}</div>
+        <div className="movie-info">
+          <span>Director</span>
+          <span>{data.Director}</span>
+          <span>Stars</span>
+          <span>{data.Actors}</span>
+          <span>Generes</span>
+          <span>{data.Genre}</span>
+          <span>Languages</span>
+          <span>{data.Language}</span>
+          <span>Awards</span>
+          <span>{data.Awards}</span>
+        </div>
+      </div>
+      <div className="section-right">
+        <img src={data.Poster} alt={data.Title} />
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetail;
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,7 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { APIKey } from "../../common/apis/MovieApiKey";
+import MovieApi from "../../common/apis/MovieApi";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "star wars";
+
+    const response = await MovieApi.get(
+      `?apiKey=${APIKey}&s=${movieText}&type=movie`
+    );
+    return response.data;
+  }
+);
+export const fetchAsyncShows = createAsyncThunk(
+  "movies/fetchAsyncShows",
+  async () => {
+    const seriesText = "friends";
+
+    const response = await MovieApi.get(
+      `?apiKey=${APIKey}&s=${seriesText}&type=series`
+    );
+    return response.data;
+  }
+);
+
+export const fetchAsyncMovieOrShowsDetails = createAsyncThunk(
+  "movies/fetchAsyncMovieOrShowsDetails",
+  async (id) => {
+    const response = await MovieApi.get(`?apiKey=${APIKey}&i=${id}&Plot==full`);
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
+  shows: {},
+  detail: {},
 };
 
 const movieSlice = createSlice({
@@ -12,8 +47,31 @@ const movieSlice = createSlice({
       state.movies = payload;
     },
   },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfuly");
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Rejected");
+    },
+    [fetchAsyncShows.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfuly");
+      return { ...state, shows: payload };
+    },
+    [fetchAsyncMovieOrShowsDetails.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfuly");
+      return { ...state, detail: payload };
+    },
+  },
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
+export const getDetail = (state) => state.movies.detail;
+
 export default movieSlice.reducer;
